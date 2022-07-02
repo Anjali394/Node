2e0f37cb73559d5798d8b9fcac046a84267206f2
Anjali Arya <aanjali@athenahealth.com>
2022-07-02 11:05:02 +0530

    create get message service

--- Browser App/server.js
+++ Browser App/server.js
@@ -1,6 +1,14 @@
 var express = require("express");
 var app = express();
 app.use(express.static(__dirname));
+
+var messages = [
+    {name: 'Anjali', message: 'Hi'},
+    {name: 'Manish', message: 'Hey'},
+]
+app.get('/messages', (req,res)=>{
+    res.send(messages);
+})
 var server = app.listen(3000, () => {
     console.log("server is listening on port : ", server.address().port)
 });
